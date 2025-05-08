ead7bafda712c3b9b92a9dade50df6e744889f1a
Harshal Rembhotkar <harshalrembhotkar94@gmail.com>
2025-05-09 00:27:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "195.88.71.182",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
